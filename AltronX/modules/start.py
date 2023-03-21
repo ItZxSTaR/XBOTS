18271dfccccde2302bc163c83b9d02824fe1f4d1
Dillu1984 <103883191+Dillu1984@users.noreply.github.com>
2023-03-21 19:18:14 +0530

    Update start.py

--- AltronX/modules/start.py
+++ AltronX/modules/start.py
@@ -8,11 +8,11 @@ PythonButton = [
         Button.inline("• ᴄᴏᴍᴍᴀɴᴅs •", data="help_back")
         ],
         [
-        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/TheAltron"),
-        Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/AltronChats")
+        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/ogsupportchat"),
+        Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/ogsupportchat")
         ],
         [
-        Button.url("• ʀᴇᴘᴏ •", "https://github.com/ItZxSTaR/TheBotSpam")
+        Button.url("• ʀᴇᴘᴏ •", "https://github.com/Dillu1984/TheBotSpam?organization=Dillu1984&organization=Dillu1984")
         ]
         ]
 
@@ -34,11 +34,11 @@ async def start(event):
         BotName = AltBot.first_name
         BotId = AltBot.id
         TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{BotName}](tg://user?id={BotId})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
-        TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [✘](https://t.me/ItzExStar)**\n\n"
+        TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [✘](https://t.me/og_punjabi)**\n\n"
         TEXT += f"» **ʙᴏᴛ ꜱᴘᴀᴍ ᴠᴇʀsɪᴏɴ :** `M3.2`\n"
         TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{telethon.__version__}`\n━━━━━━━━━━━━━━━━━"
         await event.client.send_file(
                 event.chat_id,
-                "https://te.legra.ph/file/07d39b85c6cea32f15259.jpg",
+                "https://graph.org/file/5d173672cc639e8ab4523.jpg",
                 caption=TEXT, 
                 buttons=PythonButton)
