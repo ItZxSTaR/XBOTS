72974fdbd2bea0f42d00b6fc44407e76fe845dc9
DARKRAVANREPO <142862717+DARKRAVANREPO@users.noreply.github.com>
2023-08-23 18:48:15 +0530

    Update spam.py

--- AltBots/modules/spam.py
+++ AltBots/modules/spam.py
@@ -1,4 +1,4 @@
-# © @PyXen
+# © @Dark_Ravan_01
 import asyncio
 
 from AltBots.data import GROUP, PORMS
@@ -80,7 +80,7 @@ async def spam(event: events):
 async def pspam(event):
     if event.sender_id in SUDO_USERS:
         if event.chat_id in GROUP:
-            await event.reply("» ꜱᴏʀʀʏ, ᴛʜɪꜱ ɪꜱ ᴀʟᴛʀᴏɴ ᴘʀᴏᴛᴇᴄᴛᴇᴅ ɢʀᴏᴜᴘ.")
+            await event.reply("» BABY, THIS IS WBN PROTECTED GROUP.")
         else:
             try:
                 counter = int(event.text.split(" ", 2)[1])
@@ -108,7 +108,7 @@ async def pspam(event):
 async def hang(e):
     if e.sender_id in SUDO_USERS:
         if e.chat_id in GROUP:
-            await e.reply("» ꜱᴏʀʀʏ, ᴛʜɪꜱ ɪꜱ ᴀʟᴛʀᴏɴ ᴘʀᴏᴛᴇᴄᴛᴇᴅ ɢʀᴏᴜᴘ.")
+            await e.reply("»  BABY, THIS IS WBN PROTECTED GROUP.")
         else:
             try:
                 counter = int(e.text.split(" ", 2)[1])
